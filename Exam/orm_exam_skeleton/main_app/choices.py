b764c50dface4d826b4fa45c0f4fda8d143a13bc
Emil Denkov <emildenkov2005@gmail.com>
2024-08-03 21:15:19 +0300

    Delete Exam/orm_exam_skeleton directory

--- Exam/orm_exam_skeleton/main_app/choices.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class StatusChoices(models.TextChoices):
-    PLANNED = 'Planned', 'Planned'
-    ONGOING = 'Ongoing', 'Ongoing'
-    COMPLETED = 'Completed', 'Completed'
--- Exam/orm_exam_skeleton/main_app/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def only_digits_validator(value):
-    for char in value:
-        if not char.isdigit():
-            raise ValidationError('Only digits allowed')
